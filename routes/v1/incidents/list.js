91becd4c0b889381211fadb051354d93bd6c0727
Lance Crisang <lancecrisang14@gmail.com>
2022-11-06 19:39:59 +0800

    Remove commented-out code

--- routes/v1/incidents/list.js
+++ routes/v1/incidents/list.js
@@ -72,47 +72,4 @@ router.get("/", async (req, res) => {
   );
 });
 
-// router.get("/:accountId", async (req, res) => {
-//   const token = req.headers.authorization;
-//   const limit = Math.min(req.query.limit ?? 10, 100);
-//   const location = req.query.location;
-//   const buildingId = req.query.buildingId;
-//   const severityStatus = req.query.severityStatus;
-//   const inspectorId = req.params.accountId;
-//   const resolved = req.query.resolved;
-//   const maxPageOffset = Math.max(
-//     0,
-//     Math.floor(
-//       (await countIncidents(
-//         location,
-//         buildingId,
-//         severityStatus,
-//         inspectorId,
-//         resolved
-//       )) /
-//         limit -
-//         1
-//     )
-//   );
-//   const pageOffset = Math.min(
-//     Math.max(req.query.pageOffset ?? 0, 0),
-//     maxPageOffset
-//   );
-//   const results = await fetchIncidents(
-//     true,
-//     pageOffset,
-//     limit,
-//     location,
-//     buildingId,
-//     severityStatus,
-//     inspectorId,
-//     resolved
-//   );
-//   if (results === null) {
-//     generalInternalError(req, res);
-//     return;
-//   }
-//   sendListOfReports(res, token, results, pageOffset, maxPageOffset, limit);
-// });
-
 export default router;
