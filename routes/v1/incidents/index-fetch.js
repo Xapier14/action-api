dd67422622fa61903d6cdb00d5e4919e95fee108
Lance Crisang <lancecrisang14@gmail.com>
2022-11-13 12:50:31 +0800

    Add try-catch on incident-index fetch

--- routes/v1/incidents/index-fetch.js
+++ routes/v1/incidents/index-fetch.js
@@ -21,41 +21,46 @@ router.get("/:id", async (req, res) => {
   const reportId = req.params.id;
   const token = req.headers.authorization;
   // get incident
-  const incident = await IncidentSchema.findOne({ _id: reportId });
-  if (incident === null) {
+  try {
+    const incident = await IncidentSchema.findOne({ _id: reportId });
+    if (incident === null) {
+      incidentNotFound(res, reportId);
+      return;
+    }
+    // send
+    incidentFound(res, {
+      id: incident.id,
+      inspectorId: incident.inspectorId,
+      inspectedDateTime: incident.inspectedDateTime,
+      location: incident.location,
+      buildingId: incident.buildingId,
+      collapsedStructure: incident.collapsedStructure,
+      leaningOrOutOfPlumb: incident.leaningOrOutOfPlumb,
+      damageToPrimaryStructure: incident.damageToPrimaryStructure,
+      fallingHazards: incident.fallingHazards,
+      groundMovementOrSlope: incident.groundMovementOrSlope,
+      damagedSubmergedFixtures: incident.damagedSubmergedFixtures,
+      proximityRiskTitle: incident.proximityRiskTitle,
+      proximityRisk: incident.proximityRisk,
+      evaluationComment: incident.evaluationComment,
+      estimatedBuildingDamage: incident.estimatedBuildingDamage,
+      inspectedPlacard: incident.inspectedPlacard,
+      restrictedPlacard: incident.restrictedPlacard,
+      unsafePlacard: incident.unsafePlacard,
+      barricadeNeeded: incident.barricadeNeeded,
+      barricadeComment: incident.barricadeComment,
+      detailedEvaluationNeeded: incident.detailedEvaluationNeeded,
+      detailedEvaluationAreas: incident.detailedEvaluationAreas,
+      otherRecommendations: incident.otherRecommendations,
+      furtherComments: incident.furtherComments,
+      attachments: incident.attachments,
+      resolved: incident.resolved,
+      severityStatus: incident.severityStatus,
+    });
+  } catch (err) {
+    console.log(err);
     incidentNotFound(res, reportId);
-    return;
   }
-  // send
-  incidentFound(res, {
-    id: incident.id,
-    inspectorId: incident.inspectorId,
-    inspectedDateTime: incident.inspectedDateTime,
-    location: incident.location,
-    buildingId: incident.buildingId,
-    collapsedStructure: incident.collapsedStructure,
-    leaningOrOutOfPlumb: incident.leaningOrOutOfPlumb,
-    damageToPrimaryStructure: incident.damageToPrimaryStructure,
-    fallingHazards: incident.fallingHazards,
-    groundMovementOrSlope: incident.groundMovementOrSlope,
-    damagedSubmergedFixtures: incident.damagedSubmergedFixtures,
-    proximityRiskTitle: incident.proximityRiskTitle,
-    proximityRisk: incident.proximityRisk,
-    evaluationComment: incident.evaluationComment,
-    estimatedBuildingDamage: incident.estimatedBuildingDamage,
-    inspectedPlacard: incident.inspectedPlacard,
-    restrictedPlacard: incident.restrictedPlacard,
-    unsafePlacard: incident.unsafePlacard,
-    barricadeNeeded: incident.barricadeNeeded,
-    barricadeComment: incident.barricadeComment,
-    detailedEvaluationNeeded: incident.detailedEvaluationNeeded,
-    detailedEvaluationAreas: incident.detailedEvaluationAreas,
-    otherRecommendations: incident.otherRecommendations,
-    furtherComments: incident.furtherComments,
-    attachments: incident.attachments,
-    resolved: incident.resolved,
-    severityStatus: incident.severityStatus,
-  });
 });
 
 export default router;
